3442ba14daa79a46e3c87ae6659c2d9fcda64f2d
Michael Petrinolis <michael.petrinolis@outlook.com>
2019-03-20 18:05:53 +0200

    initial commit

---
+++ CSSMap.OrchardCore/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "CSSMap.OrchardCore",
+    Author = "Michael Petrinolis",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "Integrate CSSMap Plugin to OrchardCore",
+    Category = "Content Management"
+)]
---
+++ CSSMap.OrchardCore/Migrations.cs
@@ -0,0 +1,26 @@
+﻿using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.Data.Migration;
+using OrchardCore.ContentManagement;
+using System;
+using OrchardCore.Recipes.Services;
+using System.Threading.Tasks;
+
+namespace CSSMap.OrchardCore
+{
+    public class Migrations : DataMigration
+    {
+        private readonly IRecipeMigrator _recipeMigrator;
+
+        public Migrations(IRecipeMigrator recipeMigrator)
+        {
+            _recipeMigrator = recipeMigrator;
+        }
+
+        public async Task<int> CreateAsync()
+        {
+            await _recipeMigrator.ExecuteAsync("migration.recipe.json", this);
+            return 1;
+        }
+    }
+}
